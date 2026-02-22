bdd5c5af9d6100362c2644d88d58a17ecd4c4ad2
Vinay <vinaybharatpenumaka@gmail.com>
2026-02-22 19:54:05 +0530

    added frontend

--- server.js
+++ server.js
@@ -277,6 +277,14 @@ app.delete('/api/admin/users/:id', authenticateToken, (req, res) => {
 
 // ==================== START SERVER ====================
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
